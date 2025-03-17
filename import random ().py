7366b9268a29409d2fe9a122e46628eb377cbec2
lomerby <yekinoramon191@gmail.com>
2025-03-17 11:56:48 +0100

    quiz

--- import random ().py
+++ import random ().py
@@ -1,5 +1,6 @@
 import random
 task = {1: "A" , 2: "C",3: "C",4:"D", 5:"C"}
+ran= ["a","b","c","d"]
 b = (0)
 x = [1, 2, 3, 4, 5]
 counter =(0)
@@ -9,48 +10,63 @@ while b < 5:
         b = b + 1
         if i == 1:
             qst1=input("whats the larget planet in out solar system :\n A)jupiter\nB)Mars\nC)Earth\nD)Pluto")
-            if qst1 == "a":
-                print ("correct")
-                counter = counter + 1
-            else :
-                print ("wrong")    
+            if qst1.lower in ran:
+                if qst1.lower == "a":
+                    print ("correct")
+                    counter = counter + 1
+                else :
+                    print ("wrong")  
+            else: print ("invalid input")          
 
         
         if i == 2:
             b = b + 1
             qst2=input("whats the larget number :\n A)201\nB)40\nC)50000\nD)30")
-            if qst2 == "c":
-                print ("correct")
-                counter = counter + 1
-            else :
-                print ("wrong")         
+            if qst2.lower in ran:
+                if qst2.lower == "c":
+                    print ("correct")
+                    counter = counter + 1
+                else :
+                    print ("wrong")        
+            else:
+                print("invalid input")         
 
     
     
         if i == 3:
             b = b + 1
             qst3=input("whats the smallest number :\n A)100\nB)40\nC)5\nD)30")
-            if qst3 == "c":
-                print ("correct")
-                counter = counter + 1
-            else :
-                print ("wrong")          
+            if qst3.lower in ran:
+                if qst3.lower == "c":
+                    print ("correct")
+                    counter = counter + 1
+                else :
+                    print ("wrong") 
+            else:
+                print("invalid input")                   
             
         if i == 4:
             b = b + 1
             qst4=input("whats the even number :\n A)201\nB)403\nC)500001\nD)30")
-            if qst4 == "d":
-                print ("correct")
-                counter = counter + 1
-            else :
-                print ("wrong")  
+            if qst4.lower in ran:
+
+                if qst4.lower == "d":
+                    print ("correct")
+                    counter = counter + 1
+                else :
+                    print ("wrong") 
+            else:
+                print("invalid input")             
 
         if i == 5:
             b = b + 1
             qst5=input("whats the odd number :\n A)20\nB)40\nC)500001\nD)30")
-            if qst5 == "c":
-                print ("correct")
-                counter = counter + 1
-            else :
-                print ("wrong")              
+            if qst5.lower in ran:
+                if qst5.lower == "c":
+                    print ("correct")
+                    counter = counter + 1
+                else :
+                    print ("wrong")   
+            else:
+                print("invalid input")                      
 print ("your score is :",counter)
